8590f1956b1dedc39546c5f1e503c0bcccfb10da
Petrik Lajos Szg <info@petrik.hu>
2019-03-04 15:13:12 +0100

    dffdgd

---
+++ repcsi_forgalom/JaratTest.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace repcsi_forgalom.Test
+{
+    [TestFixture]
+    public class JaratTest
+    {
+        Jaratkezelo jaratkezelo;
+        [SetUp]
+        public void Setup()
+        {
+            jaratkezelo = new Jaratkezelo();
+        }
+
+        [Test]
+        public void UjJaratTest()
+        {
+            jaratkezelo.UjJarat("262", "Kiskunfélegyháza", "Kishalas", new DateTime(2019, 03, 04));          
+        }
+
+        [Test]
+        public void MikorIndulTest()
+        {
+            jaratkezelo.UjJarat("262", "Kiskunfélegyháza", "Kishalas", new DateTime(2019, 03, 04));
+            Assert.AreEqual(new DateTime(2019, 03, 04), jaratkezelo.MikorIndul("262"));
+        }
+        [Test]
+        public void jaratokRepuloterrolTest()
+        {
+            jaratkezelo.UjJarat("262", "Kiskunfélegyháza", "Kishalas", new DateTime(2019, 03, 04));
+            var elvartLista = new List<string>();
+            elvartLista.Add("262");
+            Assert.AreEqual(elvartLista, jaratkezelo.jaratokRepuloterrol("Kiskunfélegyháza"));
+        }
+        [Test]
+        public void Ketugyanolyanjarat()
+        {
+            jaratkezelo.UjJarat("262", "Kiskunfélegyháza", "Kishalas", new DateTime(2019, 03, 04));
+            Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    jaratkezelo.UjJarat("262", "Kiskunfélegyháza", "Kishalas", new DateTime(2019, 03, 04));
+                }
+                );
+        }
+    }
+}
---
+++ repcsi_forgalom/Jaratkezelo.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repcsi_forgalom
+{
+    class Jaratkezelo
+    {
+        public class Jarat
+        {
+            public string jaratSzam { get; set; }           
+            public string honnanRepter { get; set; }
+            public string hovaRepter { get; set; }
+            public DateTime indulas { get; set; }
+            public int keses;
+
+            public Jarat(string jaratSzam, string honnanRepter, string hovaRepter, DateTime indulas)
+            {
+                this.jaratSzam = jaratSzam;
+                this.honnanRepter = honnanRepter;
+                this.hovaRepter = hovaRepter;
+                this.indulas = indulas;
+                this.keses = 0;
+            }
+        }
+        List<Jarat> jaratok = new List<Jarat>();
+
+        public void UjJarat(string jaratSzam, string honnanRepter, string hovaRepter, DateTime indulas)
+        {
+            var r = new Jarat(jaratSzam, honnanRepter, hovaRepter, indulas);
+            foreach (var item in jaratok)
+            {
+                if (item.jaratSzam.Equals(jaratSzam))
+                {
+                    throw new ArgumentException("már van ilyen járat");
+                }
+            }
+            jaratok.Add(r);
+
+
+        }
+        public DateTime MikorIndul(string jaratSzam)
+        {
+            foreach (var jarat in jaratok)
+            {
+                if (jaratSzam.Equals(jarat.jaratSzam))
+                {
+                    return jarat.indulas.AddMinutes(jarat.keses);
+                }
+            }
+            throw new ArgumentException("Nincs ilyen járat");
+        }
+
+        public List<string> jaratokRepuloterrol(string repter)
+        {
+            List<string> jaratokLista = new List<string>();
+            foreach (var item in jaratok)
+            {
+                if (repter.Equals(item.honnanRepter))
+                {
+                    jaratokLista.Add(item.jaratSzam);
+                }
+            }
+            if (jaratokLista.Count==0)
+            {
+                throw new ArgumentException();
+            }
+            return jaratokLista;
+        }
+        public void Keses(string jaratszam, int keses)
+        {
+            foreach (var item in jaratok)
+            {
+                if (jaratszam.Equals(item.jaratSzam))
+                {
+                    if ((item.keses+keses)<0)
+                    {
+                        throw new NegativKesesException(keses);
+                    }
+                    else
+                    {
+                        item.keses += keses;
+                    }
+                }
+            }
+        }
+        class NegativKesesException : Exception
+        {
+            public NegativKesesException(int keses)
+                    : base("nemtom " + keses)
+            {
+
+            }
+        }
+    }
+}
